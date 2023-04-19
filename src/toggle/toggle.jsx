77af3465df11eae7ad727d350210e1c36a03cb48
dsdy <dsdy06@gmail.com>
2023-04-19 12:08:58 +0300

    work in progress

--- src/toggle/toggle.jsx
+++ src/toggle/toggle.jsx
@@ -4,7 +4,11 @@ import "../style/toggle.css"
 export default function Toggle() {
   return (
     <div className="toggle">
-      <div className="toggle__background" />
+      <div className="circle">
+        <div className="circle__inner-halo" />
+        <div className="circle__middle-halo" />
+        <div className="circle__outer-halo" />
+      </div>
     </div>
   )
 }
