2164459c2527e32dd42dea1d061c210150f06b72
dsdy <dsdy06@gmail.com>
2023-04-19 10:55:41 +0300

    work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import './style/App.css'
+import React from "react"
+import "./style/App.css"
 import "./style/background.css"
+import Toggle from "./toggle/toggle"
 
 function App() {
   return (
     <div className="App">
-      <div className="background"> ee</div>
+      <div className="background">
+        <div className="position-block">
+          <Toggle />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/toggle/toggle.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "../style/toggle.css"
+
+export default function Toggle() {
+  return (
+    <div className="toggle">
+      <div className="toggle__background" />
+    </div>
+  )
+}
