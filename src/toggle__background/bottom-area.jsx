5163e380811a674900c56d36d7c964e528ee2dd6
dsdy <dsdy06@gmail.com>
2023-04-20 21:34:56 +0300

    moved to external file

---
+++ src/toggle__background/bottom-area.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import "../style/bottom-area.css"
+
+export default function BottomArea() {
+  return (
+    <div className="bottom-area">
+      <div className="bottom-area__item bottom-area__item--rear bottom-area__item--top-right" />
+      <div className="bottom-area__item bottom-area__item--rear bottom-area__item--bottom-right" />
+      <div className="bottom-area__item bottom-area__item--rear bottom-area__item--right-center" />
+      <div className="bottom-area__item bottom-area__item--rear bottom-area__item--center" />
+      <div className="bottom-area__item bottom-area__item--rear bottom-area__item--left-center" />
+      <div className="bottom-area__item bottom-area__item--rear bottom-area__item--top-left" />
+      <div className="bottom-area__item bottom-area__item--rear bottom-area__item--bottom-left" />
+      <div className="bottom-area__item bottom-area__item--front bottom-area__item--top-right" />
+      <div className="bottom-area__item bottom-area__item--front bottom-area__item--bottom-right" />
+      <div className="bottom-area__item bottom-area__item--front bottom-area__item--right-center" />
+      <div className="bottom-area__item bottom-area__item--front bottom-area__item--center" />
+      <div className="bottom-area__item bottom-area__item--front bottom-area__item--left-center" />
+      <div className="bottom-area__item bottom-area__item--front bottom-area__item--top-left" />
+      <div className="bottom-area__item bottom-area__item--front bottom-area__item--bottom-left" />
+    </div>
+  )
+}
--- src/toggle__background/toggle__background.jsx
+++ src/toggle__background/toggle__background.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import "../style/toggle-background.css"
+import BottomArea from "./bottom-area"
 
 export default function ToggleBackground() {
   return (
@@ -7,38 +8,7 @@ export default function ToggleBackground() {
       <div className="top-area">
         <div className="top-cluster" />
       </div>
-      <div className="bottom-area">
-        <div className="bottom-area__item bottom-area__item--rear bottom-area__item--top-right" />
-        <div className="bottom-area__item bottom-area__item--rear bottom-area__item--bottom-right" />
-        <div className="bottom-area__item bottom-area__item--rear bottom-area__item--right-center" />
-        <div className="bottom-area__item bottom-area__item--rear bottom-area__item--center" />
-        <div className="bottom-area__item bottom-area__item--rear bottom-area__item--left-center" />
-        <div className="bottom-area__item bottom-area__item--rear bottom-area__item--top-left" />
-        <div className="bottom-area__item bottom-area__item--rear bottom-area__item--bottom-left" />
-        <div className="bottom-area__item bottom-area__item--front bottom-area__item--top-right" />
-        <div className="bottom-area__item bottom-area__item--front bottom-area__item--bottom-right" />
-        <div className="bottom-area__item bottom-area__item--front bottom-area__item--right-center" />
-        <div className="bottom-area__item bottom-area__item--front bottom-area__item--center" />
-        <div className="bottom-area__item bottom-area__item--front bottom-area__item--left-center" />
-        <div className="bottom-area__item bottom-area__item--front bottom-area__item--top-left" />
-        <div className="bottom-area__item bottom-area__item--front bottom-area__item--bottom-left" />
-        {/* <div className="bottom-cluster bottom-cluster--rear">
-           <div className="bottom-cluster__circle bottom-cluster__circle--top-right" />
-        <div className="bottom-cluster__circle bottom-cluster__circle--bottom-right" />
-        <div className="bottom-cluster__circle bottom-cluster__circle--right-center" />
-        <div className="bottom-cluster__circle bottom-cluster__circle--center" />
-        <div className="bottom-cluster__circle bottom-cluster__circle--left-center" />
-        <div className="bottom-cluster__circle bottom-cluster__circle--left" /> 
-        </div> 
-         <div className="bottom-cluster bottom-cluster--front">
-          <div className="bottom-cluster__circle bottom-cluster__circle--top-right" />
-          <div className="bottom-cluster__circle bottom-cluster__circle--bottom-right" />
-          <div className="bottom-cluster__circle bottom-cluster__circle--right-center" />
-          <div className="bottom-cluster__circle bottom-cluster__circle--center" />
-          <div className="bottom-cluster__circle bottom-cluster__circle--left-center" />
-          <div className="bottom-cluster__circle bottom-cluster__circle--left" />
-        </div> */}
-      </div>
+      <BottomArea />
     </div>
   )
 }
