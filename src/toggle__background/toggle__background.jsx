55633e124d82b777e8cd168c913a7214460bac8b
dsdy <dsdy06@gmail.com>
2023-04-20 18:35:11 +0300

    work in progress

--- src/toggle__background/toggle__background.jsx
+++ src/toggle__background/toggle__background.jsx
@@ -4,13 +4,26 @@ import "../style/toggle-background.css"
 export default function ToggleBackground() {
   return (
     <div className="toggle__background">
-      <div className="bottom-cluster bottom-cluster--rear">
-        <div className="bottom-cluster__circle bottom-cluster__circle--top-right" />
+      <div className="top-area">
+        <div className="top-cluster" />
+      </div>
+      <div className="bottom-area">
+        <div className="bottom-cluster bottom-cluster--rear">
+          {/* <div className="bottom-cluster__circle bottom-cluster__circle--top-right" />
         <div className="bottom-cluster__circle bottom-cluster__circle--bottom-right" />
         <div className="bottom-cluster__circle bottom-cluster__circle--right-center" />
         <div className="bottom-cluster__circle bottom-cluster__circle--center" />
         <div className="bottom-cluster__circle bottom-cluster__circle--left-center" />
-        <div className="bottom-cluster__circle bottom-cluster__circle--left" />
+        <div className="bottom-cluster__circle bottom-cluster__circle--left" /> */}
+        </div>
+        <div className="bottom-cluster bottom-cluster--front">
+          {/* <div className="bottom-cluster__circle bottom-cluster__circle--top-right" />
+        <div className="bottom-cluster__circle bottom-cluster__circle--bottom-right" />
+        <div className="bottom-cluster__circle bottom-cluster__circle--right-center" />
+        <div className="bottom-cluster__circle bottom-cluster__circle--center" />
+        <div className="bottom-cluster__circle bottom-cluster__circle--left-center" />
+        <div className="bottom-cluster__circle bottom-cluster__circle--left" /> */}
+        </div>
       </div>
     </div>
   )
