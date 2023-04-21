f96ef61d4f27d703f2f23e0ab6be245fcec8b211
dsdy <dsdy06@gmail.com>
2023-04-21 22:57:45 +0300

    stars are done

--- src/toggle__background/toggle__background.jsx
+++ src/toggle__background/toggle__background.jsx
@@ -15,6 +15,30 @@ export default function ToggleBackground() {
         <div className="top-area__item top-area__item--top-left-bottom">
           <span>&#10022;</span>
         </div>
+        <div className="top-area__item top-area__item--bottom-left-top">
+          <span>&#10022;</span>
+        </div>
+        <div className="top-area__item top-area__item--bottom-left-left">
+          <span>&#10022;</span>
+        </div>
+        <div className="top-area__item top-area__item--bottom-left-bottom">
+          <span>&#10022;</span>
+        </div>
+        <div className="top-area__item top-area__item--right-top-left">
+          <span>&#10022;</span>
+        </div>
+        <div className="top-area__item top-area__item--right-top-right">
+          <span>&#10022;</span>
+        </div>
+        <div className="top-area__item top-area__item--right-mid-right">
+          <span>&#10022;</span>
+        </div>
+        <div className="top-area__item top-area__item--right-mid-left">
+          <span>&#10022;</span>
+        </div>
+        <div className="top-area__item top-area__item--right-bottom">
+          <span>&#10022;</span>
+        </div>
       </div>
       <BottomArea />
     </div>
