40c9320d76e6d1c46698bb7fd3a07caf3b78f624
dsdy <dsdy06@gmail.com>
2023-04-22 10:30:39 +0300

    moved toparea to external file

--- src/toggle__background/toggle__background.jsx
+++ src/toggle__background/toggle__background.jsx
@@ -1,45 +1,12 @@
 import React from "react"
 import "../style/toggle-background.css"
 import BottomArea from "./bottom-area"
+import TopArea from "./top-area"
 
 export default function ToggleBackground() {
   return (
     <div className="toggle__background">
-      <div className="top-area">
-        <div className="top-area__item top-area__item--top-left-top">
-          <span>&#10022;</span>
-        </div>
-        <div className="top-area__item top-area__item--top-left-left">
-          <span>&#10022;</span>
-        </div>
-        <div className="top-area__item top-area__item--top-left-bottom">
-          <span>&#10022;</span>
-        </div>
-        <div className="top-area__item top-area__item--bottom-left-top">
-          <span>&#10022;</span>
-        </div>
-        <div className="top-area__item top-area__item--bottom-left-left">
-          <span>&#10022;</span>
-        </div>
-        <div className="top-area__item top-area__item--bottom-left-bottom">
-          <span>&#10022;</span>
-        </div>
-        <div className="top-area__item top-area__item--right-top-left">
-          <span>&#10022;</span>
-        </div>
-        <div className="top-area__item top-area__item--right-top-right">
-          <span>&#10022;</span>
-        </div>
-        <div className="top-area__item top-area__item--right-mid-right">
-          <span>&#10022;</span>
-        </div>
-        <div className="top-area__item top-area__item--right-mid-left">
-          <span>&#10022;</span>
-        </div>
-        <div className="top-area__item top-area__item--right-bottom">
-          <span>&#10022;</span>
-        </div>
-      </div>
+      <TopArea />
       <BottomArea />
     </div>
   )
---
+++ src/toggle__background/top-area.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import "../style/top-area.css"
+
+export default function TopArea() {
+  return (
+    <div className="top-area">
+      <div className="top-area__item top-area__item--top-left-top">
+        <span>&#10022;</span>
+      </div>
+      <div className="top-area__item top-area__item--top-left-left">
+        <span>&#10022;</span>
+      </div>
+      <div className="top-area__item top-area__item--top-left-bottom">
+        <span>&#10022;</span>
+      </div>
+      <div className="top-area__item top-area__item--bottom-left-top">
+        <span>&#10022;</span>
+      </div>
+      <div className="top-area__item top-area__item--bottom-left-left">
+        <span>&#10022;</span>
+      </div>
+      <div className="top-area__item top-area__item--bottom-left-bottom">
+        <span>&#10022;</span>
+      </div>
+      <div className="top-area__item top-area__item--right-top-left">
+        <span>&#10022;</span>
+      </div>
+      <div className="top-area__item top-area__item--right-top-right">
+        <span>&#10022;</span>
+      </div>
+      <div className="top-area__item top-area__item--right-mid-right">
+        <span>&#10022;</span>
+      </div>
+      <div className="top-area__item top-area__item--right-mid-left">
+        <span>&#10022;</span>
+      </div>
+      <div className="top-area__item top-area__item--right-bottom">
+        <span>&#10022;</span>
+      </div>
+    </div>
+  )
+}
