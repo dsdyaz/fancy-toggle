fc12de95c2174bf81a5e7c279b9a6b1d82fd9e54
dsdy <dsdy06@gmail.com>
2023-04-24 21:32:03 +0300

    made back and forth animations

--- src/toggle/toggle.jsx
+++ src/toggle/toggle.jsx
@@ -1,24 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import "../style/toggle.css"
 import ToggleBackground from "../toggle__background/toggle__background"
 import Circle from "../circle/circle"
 
 export default function Toggle() {
+  const [isOff, setOff] = useState(false)
   function move() {
-    const toggle = document.getElementById("toggle")
+    console.log(isOff)
+    const backgroundAnimation = isOff ? "translateY(0%)" : "translateY(66%)"
+    const circleAnimation = isOff ? "translateX(0%)" : "translateX(160%)"
     const circle = document.getElementById("circle")
     const background = document.getElementById("toggle__background")
-    background.style.transform = "translateY(66%)"
-    circle.style.transform = "translateX(160%)"
-    circle.style.transition = "3s"
-    background.style.transition = "3s"
+    circle.className = "circle circle__off"
+    background.style.transform = backgroundAnimation
+    circle.style.transform = circleAnimation
+    circle.style.transition = "1s"
+    background.style.transition = "1s"
+    setOff(!isOff)
   }
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
     <div className="toggle" id="toggle" onClick={() => move()}>
       <div className="toggle__inner-shadow" />
       <ToggleBackground />
-      <Circle isOff />
+      <Circle />
     </div>
   )
 }
