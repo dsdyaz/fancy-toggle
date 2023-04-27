858ae09c763e4e6de5e415f117258edbc273489e
dsdy <dsdy06@gmail.com>
2023-04-27 10:52:46 +0300

    finished fixing bottomm area

--- src/toggle__background/bottom-area.jsx
+++ src/toggle__background/bottom-area.jsx
@@ -13,13 +13,15 @@ export default function BottomArea() {
         <div className="bottom-area__item bottom-area__item_top-left" />
         <div className="bottom-area__item bottom-area__item_bottom-left" />
       </div>
-      <div className="bottom-area__item bottom-area__item--front bottom-area__item--top-right" />
-      <div className="bottom-area__item bottom-area__item--front bottom-area__item--bottom-right" />
-      <div className="bottom-area__item bottom-area__item--front bottom-area__item--right-center" />
-      <div className="bottom-area__item bottom-area__item--front bottom-area__item--center" />
-      <div className="bottom-area__item bottom-area__item--front bottom-area__item--left-center" />
-      <div className="bottom-area__item bottom-area__item--front bottom-area__item--top-left" />
-      <div className="bottom-area__item bottom-area__item--front bottom-area__item--bottom-left" />
+      <div className="bottom-area__front-cluster">
+        <div className="bottom-area__item bottom-area__item_top-right" />
+        <div className="bottom-area__item bottom-area__item_bottom-right" />
+        <div className="bottom-area__item bottom-area__item_right-center" />
+        <div className="bottom-area__item bottom-area__item_center" />
+        <div className="bottom-area__item bottom-area__item_left-center" />
+        <div className="bottom-area__item bottom-area__item_top-left" />
+        <div className="bottom-area__item bottom-area__item_bottom-left" />
+      </div>
     </div>
   )
 }
