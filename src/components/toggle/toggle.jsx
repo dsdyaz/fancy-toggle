c45db866dac1805e5775a7f445b24597567438f7
dsdy <dsdy06@gmail.com>
2023-05-01 11:02:06 +0300

    remade animation func

--- src/components/toggle/toggle.jsx
+++ src/components/toggle/toggle.jsx
@@ -6,12 +6,8 @@ import Circle from "../circle/circle"
 export default function Toggle() {
   const [mode, setMode] = useState("dark")
   function move() {
-    const circle = document.getElementById("circle")
-    const darkCircle = document.getElementById("circle_off")
-    const background = document.getElementById("toggle__background")
-    circle.className = `toggle__circle toggle__circle_${mode}`
-    darkCircle.className = `circle__background circle__background_off circle__background_${mode}`
-    background.className = `toggle__background toggle__background_${mode}`
+    const toggle = document.getElementById("toggle")
+    toggle.className = `toggle toggle_${mode}`
     if (mode === "light") {
       setMode("dark")
     } else if (mode === "dark") {
